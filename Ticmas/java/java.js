d92b0dd3b77e86f4be59e55191d3c103db89536c
alejandro allende <aallende11@gmail.com>
2023-01-29 04:13:46 -0300

    version4

---
+++ Ticmas/java/java.js
@@ -0,0 +1,6 @@
+function pasarhoja() {
+
+    document.getElementById('todo').setAttribute('style', 'display: flex');
+    document.getElementById('carta').setAttribute('style', 'display: none');
+    
+}
