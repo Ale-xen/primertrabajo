b44f3cf8a71a140a8980a47ec7822ce264c41dbb
alejandro allende <aallende11@gmail.com>
2023-02-02 03:07:20 -0300

    cambios finales2

--- Ticmas/java/java.js
+++ Ticmas/java/java.js
@@ -4,3 +4,19 @@ function pasarhoja() {
     document.getElementById('carta').setAttribute('style', 'display: none');
     
 }
+
+let leermas_btn = document.getElementById('leermas_btn');
+let ocultartexto = document.getElementById('ocultartexto');
+
+leermas_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+  ocultartexto.classList.toggle('showText');
+
+  if(ocultartexto.classList.contains('showText')) {
+    leermas_btn.innerHTML = 'Leer menos'
+  }
+  else {
+    leermas_btn.innerHTML = 'leer mas'
+  }
+}
